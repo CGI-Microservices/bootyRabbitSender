0af382e09ef29ec685d9e52aac65f5c850d13793
Daniel Beyer <danielbeyer@cgi.com>
2017-11-12 19:23:15 +0100

    changes for the rest-sender

---
+++ src/main/java/com/ohnet/bootyrabbit/to/Content.java
@@ -0,0 +1,31 @@
+package com.ohnet.bootyrabbit.to;
+
+import java.util.UUID;
+
+public class Content {
+
+	private UUID id;
+	private String content;
+	
+	public Content() {
+		
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+
+	
+}
